8ab76c571a45d27db017245d4aceabadde088f10
Thales Duarte <thalesduarte05@gmail.com>
2025-03-28 22:04:44 -0300

    Parei em validação dos dados do post

--- src/hooks/useInsertDocument.jsx
+++ src/hooks/useInsertDocument.jsx
@@ -10,15 +10,16 @@ const initialState = {
 const insertReducer = (state, action) => {
   switch (action.type) {
     case 'LOADING':
-      return { loading: true, error: null }
-    case 'INSERTED':
-      return { loading: false, error: null }
+      return { loading: true, error: null };
+    case 'INSERTED_DOC':
+      return { loading: false, error: null };
     case 'ERROR':
-      return { loading: false, error: action.payload }
+      return { loading: false, error: action.payload };
     default:
-      return state
-  }
-}
+      return state;
+  };
+
+};
 
 export const useInsertDocument = (docCollection) => {
     const [response, dispatch] = useReducer(insertReducer, initialState)
@@ -26,21 +27,46 @@ export const useInsertDocument = (docCollection) => {
     // deal memory leak
 
     const [cancelled, setCancelled] = useState(false)
+    
     const checkCancelBeforeDispatch = (action) => {
-        if (!cancelled) {
-            dispatch(action)
-        }
+      if (!cancelled) {
+        dispatch(action)
+      }
     }
+    
 
     const insertDocument = async (document) => {
+      checkCancelBeforeDispatch({
+          type: 'LOADING'
+
+        });
+
+
 
         try {
+      const newDocument = { ...document, createdAt: Timestamp.now() }
+      const insertedDoc = await addDoc(collection(db, docCollection), newDocument)
 
-            const newDocument = { ...document, createdAt: Timestamp.now() }
-            const insertDocument = await addDoc(collection(db, docCollection), newDocument)
+      checkCancelBeforeDispatch({
+          type: 'INSERTED_DOC',
+          payload: insertedDoc
+        });
 
-        } catch (error) {
+      } catch (error) {
       
+      checkCancelBeforeDispatch({
+            type: 'ERROR',
+            payload: error.message
+          });
         }
-    }
+    };
+
+    
+  // sei la esse trem nao ta dando certo
+  
+    // useEffect(() => {
+    //     return () => setCancelled(true)
+    // }, []);
+
+    return { insertDocument, response }
 }
--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -2,18 +2,50 @@ import styles from './CreatePost.module.css';
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuthValue } from '../../context/AuthContext'
+import { useInsertDocument } from '../../hooks/useInsertDocument'	
 
 const CreatePost = () => {
 
   const [title, setTitle] = useState('')
   const [image, setImage] = useState('')
   const [body, setBody] = useState('')
-  const [tags, setTags] = useState('')
+  const [tags, setTags] = useState([])
   const [formError, setFormError] = useState('')
+  const { insertDocument, response } = useInsertDocument('posts')
+  const { user } = useAuthValue();
 
+  
   const handleSubmit = async (e) => {
     e.preventDefault()
     setFormError('')
+
+    //validate image URL
+    // try {
+    //   new URL(image)
+    // } catch (error) {
+    //   setFormError('A imagem precisa ser uma URL.')
+    // }
+
+    //criar um array de tags
+
+    //checar todos os campos
+
+    if (formError) return;
+
+    insertDocument ({
+      title,
+      image,
+      body,
+      tags: tags.split(','),
+      uid: user.uid,
+      createdBy: user.displayName
+    })
+
+    // redirect to home page
+
+
+
+
 
   }
 
@@ -22,26 +54,32 @@ const CreatePost = () => {
       <h2>Criar post</h2>
       <p>Escreva sobre o que você quiser e compartilhe o seu conhecimento!</p>
       <form onSubmit={handleSubmit}>
+       
         <label>
           <span>Título:</span>
           <input type="text" name='title' required placeholder='Pense no título...' onChange={(e) => setTitle(e.target.value)} value={title} />
         </label>
+       
         <label>
           <span>URL da imagem:</span>
           <input type="text" name='image' required placeholder='Insira uma imagem que representa seu post' onChange={(e) => setImage(e.target.value)} value={image} />
         </label>
+        
         <label>
           <span>Conteúdo:</span>
           <textarea name="body" required placeholder='Insira o conteúdo do seu post' onChange={(e) => setBody(e.target.value)} value={body}></textarea>
         </label>
+        
         <label>
           <span>Tags:</span>
           <input type="text" name='tags' required placeholder='Insira as Tags separadas por vírgula.' onChange={(e) => setTags(e.target.value)} value={tags} />
         </label>
-        <button className='btn'>Criar</button>
-        {/* {!loading && <button className='btn'>Cadastrar</button>}
-        {loading && <button className='btn' disabled>Aguarde...</button>}
-        {error && <p className='error'>{error}</p>} */}
+
+
+       {!response.loading && <button className="btn" >Cadastrar</button>}
+       {response.loading && <button className="btn" disabled>Aguarde...</button>}
+       {response.error && <p className='error'>{response.error}</p>}
+      
       </form>
     </div>
   )
